702e4b2b7a12561906d01b4b7050b7cb02605b7f
zsj <zsjzsj2001@qq.com>
2022-12-27 20:57:38 +0800

    first commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,9 @@
-package org.example;public class Main {
+package org.example;
+public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("hello-zsj");
+        System.out.println("11111");
+
     }
 }
